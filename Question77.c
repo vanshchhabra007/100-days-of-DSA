40a08d2fb4630c7a31b4a6a6c06bff009d4d76a7
vanshchhabra007 <vansh.22567@stu.upes.ac.in>
2026-04-28 00:42:12 +0530

    Question77.c

---
+++ Question77.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXN 100005
+
+int *graph[MAXN];
+int graphSize[MAXN];
+int visited[MAXN];
+
+void addEdge(int u, int v) {
+    graph[u] = realloc(graph[u], (graphSize[u] + 1) * sizeof(int));
+    graph[u][graphSize[u]++] = v;
+}
+
+void dfs(int node) {
+    visited[node] = 1;
+    for (int i = 0; i < graphSize[node]; i++) {
+        int neighbor = graph[node][i];
+        if (!visited[neighbor]) {
+            dfs(neighbor);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize
+    for (int i = 1; i <= n; i++) {
+        graph[i] = NULL;
+        graphSize[i] = 0;
+        visited[i] = 0;
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+        addEdge(v, u); // undirected graph
+    }
+
+    // Run DFS from node 1
+    dfs(1);
+
+    // Check if all nodes are visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+    return 0;
+}
